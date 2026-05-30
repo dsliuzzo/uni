411fa3d7bdcb1770975edd3f0b99bd7b9180d1c8
udiahfr <claudiadeferrariis@gmail.com>
2026-05-30 22:06:35 +0200

    automatica e sisop
    chiarimenti sul margine di fase e relazione con banda passante e svolgimento prova d'esame 20 gennaio 2023 (deadly)

--- code/so/src/Prova_esame_20_01_2023/CallCenter.java
+++ code/so/src/Prova_esame_20_01_2023/CallCenter.java
@@ -1,4 +1,28 @@
 package Prova_esame_20_01_2023;
 
-public class CallCenter {
+public abstract class CallCenter {
+    protected int numOperatori;
+    protected int numClienti;
+
+    abstract void richiediAssistenza() throws InterruptedException;
+    abstract void terminaChiamata() throws InterruptedException;
+    abstract void fornisciAssistenza() throws InterruptedException;
+    abstract void prossimoCliente() throws InterruptedException;
+
+    public CallCenter(int numOper, int numCli) {
+        numOperatori = numOper;
+        numClienti = numCli;
+    }
+
+    public void test(int numOperatori, int numClienti) {
+        Operatore[] operatori = new Operatore[numOperatori];
+        for (int i = 0; i < numOperatori; i++) {
+            operatori[i] = new Operatore(this, i);
+            operatori[i].setDaemon(true);
+            operatori[i].start();
+        }
+        for (int i = 0; i < numClienti; i++) {
+            new Cliente(this, i).start();
+        }
+    }
 }
