eeefe083c8ad54ad06544d4d256022d42085adc4
udiahfr <claudiadeferrariis@gmail.com>
2026-06-12 14:45:09 +0200

    cantiere partial

--- code/so/src/Prova_Cantiere_04_07_22/CantiereFerroviarioA.java
+++ code/so/src/Prova_Cantiere_04_07_22/CantiereFerroviarioA.java
@@ -1,4 +1,21 @@
 package Prova_Cantiere_04_07_22;
 
-public class CantiereFerroviarioA {
+import Prova_callcenter_20_01_2023.Operatore;
+
+public abstract class CantiereFerroviarioA {
+    public static final int M = 3;
+    public static final int nT = 4;
+    public static final int nR = 4;
+
+    public abstract void lavora(int t) throws InterruptedException;
+    public abstract void termina(int t) throws InterruptedException;
+
+    public void test(){
+        for(int i = 0; i < nT; i++){
+            new Operaio(this, i,0).start();
+        }
+        for(int i = 0; i < nR; i++){
+            new Operaio(this,i,1);
+        }
+    }
 }
--- code/so/src/Prova_Cantiere_04_07_22/Operaio.java
+++ code/so/src/Prova_Cantiere_04_07_22/Operaio.java
@@ -1,4 +1,31 @@
 package Prova_Cantiere_04_07_22;
 
-public class Operaio {
+import java.util.concurrent.TimeUnit;
+
+public class Operaio extends Thread{
+    private CantiereFerroviarioA cantiere;
+    private int id;
+    private int tipo;
+
+    public Operaio(CantiereFerroviarioA cantiere, int id, int tipo) {
+        this.cantiere = cantiere;
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public int id(){return id;}
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                TimeUnit.SECONDS.sleep(tipo == 0? 2:3);
+                cantiere.lavora(tipo);
+                cantiere.termina(tipo);
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
