977681b656ac7037f2cc6adf23d78980eb6bb9d1
Davide Liuzzo <dsliuzzo@gmail.com>
2026-06-13 15:58:48 +0200

    sync

--- code/so/src/Prova_Ferryboat_06_06_2022/FerryboatLC.java
+++ code/so/src/Prova_Ferryboat_06_06_2022/FerryboatLC.java
@@ -1,4 +1,13 @@
 package Prova_Ferryboat_06_06_2022;
 
-public class FerryboatLC {
+import java.util.LinkedList;
+import java.util.concurrent.locks.*;
+
+public class FerryboatLC extends Ferryboat {
+    private Lock l = new ReentrantLock();
+    private Condition attesaEntrata = l.newCondition();
+    private Condition attesaUscita = l.newCondition();
+    private LinkedList<Thread> fifo = new LinkedList<>();
+    private LinkedList<Thread> lifo = new LinkedList<>();
+    private boolean possoEntrare 
 }
