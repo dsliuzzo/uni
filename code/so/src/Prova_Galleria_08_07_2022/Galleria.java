ee87bcac104fa59d2adc7c8f830aa3f68e4df5d2
Davide <dsliuzzo@gmail.com>
2026-06-16 08:54:16 +0200

    anche oggi si passa a windows pd

--- code/so/src/Prova_Galleria_08_07_2022/Galleria.java
+++ code/so/src/Prova_Galleria_08_07_2022/Galleria.java
@@ -1,5 +1,19 @@
 package Prova_Galleria_08_07_2022;
 
 public abstract class Galleria {
+    private final int numVisitatori = 100;
+    private final int numGuida = 5;
+    protected final int maxVisita = 30;
+    protected final int maxGalleria = 200;
 
+    public abstract void iniziaVisita(int lingua) throws InterruptedException;
+    public abstract void esci(int lingua) throws InterruptedException;
+    public abstract void attendiVisitatori() throws InterruptedException;
+    public abstract void terminaVisita() throws InterruptedException;
+
+    public void test()
+        for (int i = 0; i < numGuida; i++) {
+
+        }
+    }
 }
